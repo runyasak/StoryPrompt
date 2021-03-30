3570683162087594fd9b6b1b8fb789a902947803
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-03-30 22:48:29 +0700

    feat: implement outlets and events

--- StoryPrompt/ViewController.swift
+++ StoryPrompt/ViewController.swift
@@ -2,9 +2,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let storyPrompt = StoryPromptEntry()
+    
+    @IBOutlet weak var nounTextField: UITextField!
+    @IBOutlet weak var adjectiveTextField: UITextField!
+    @IBOutlet weak var verbTextField: UITextField!
+    @IBOutlet weak var numberSlider: UISlider!
+    @IBOutlet weak var numberLabel: UILabel!
+
+    @IBAction func changeNumber(_ sender: UISlider) {
+        numberLabel.text = "Number: \(Int(sender.value))"
+        storyPrompt.number = Int(sender.value)
+    }
+
+    @IBAction func changeStoryType(_ sender: UISegmentedControl) {
+        if let genre = StoryPrompts.Genre(rawValue: sender.selectedSegmentIndex) {
+            storyPrompt.genre = genre
+        } else {
+            storyPrompt.genre = .scifi
+        }
+        
+        print(storyPrompt.genre)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let storyPrompt = StoryPromptEntry()
+        
+        numberSlider.value = 7.5
         storyPrompt.noun = "toaster"
         storyPrompt.adjective = "smelly"
         storyPrompt.verb = "burps"
