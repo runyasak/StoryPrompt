b655652e0e71349eba8cf25bc88f87e290cec144
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-03-30 21:58:12 +0700

    feat: implement StoryPromptEntry

--- StoryPrompt/ViewController.swift
+++ StoryPrompt/ViewController.swift
@@ -1,19 +1,16 @@
-//
-//  ViewController.swift
-//  StoryPrompt
-//
-//  Created by Runyasak Chaengnaimuang on 30/3/2564 BE.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let storyPrompt = StoryPromptEntry()
+        storyPrompt.noun = "toaster"
+        storyPrompt.adjective = "smelly"
+        storyPrompt.verb = "burps"
+        storyPrompt.number = 10
+        print(storyPrompt)
     }
-
-
+    
 }
 
